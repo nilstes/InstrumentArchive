b21c38773032a60aa76525dc3ef4d86171e737d5
stud-nilstes <nils.tesdal@hist.no>
2015-12-17 14:22:52 +0100

    Logger til filer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.security.ProtectionDomain;
 import java.util.logging.FileHandler;
+import java.util.logging.Filter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
@@ -11,15 +18,11 @@ import org.eclipse.jetty.webapp.WebAppContext;
  */
 public class Main {
 
-    private static Logger logger = Logger.getLogger(Main.class.getName());
-
     public static void main(String[] args) throws Exception {
         
-        FileHandler fh;   
-        fh = new FileHandler("debug.log");   
-        logger.addHandler(fh); 
+        Logger logger = initLogging();
 
-        System.out.println("Starting server...");
+        logger.info("Starting server...");
         int port = Integer.parseInt(System.getProperty("port", "8080"));
         String contextPath = System.getProperty("contextPath", "instruments");
         Server server = new Server(port);
@@ -31,6 +34,42 @@ public class Main {
         server.setHandler(webapp);
         server.start();
         System.out.println("Server started...");
+        logger.info("Server started...");
         server.join();
+    }
+
+    private static Logger initLogging() throws IOException, SecurityException {
+        File logDirectory = new File("log");
+        if(!logDirectory.exists()) {
+            logDirectory.mkdir();
+        }
+        
+        // Get root logger
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.FINER);
+        
+        // Remove console handlers
+        Handler[] handlers = logger.getHandlers();
+        if (handlers[0] instanceof Handler) {
+            logger.removeHandler(handlers[0]);
+        }
+
+        // Add new file handlers
+        FileHandler debugHandler = new FileHandler("log/debug.log", 1048576, 5, true);
+        debugHandler.setFormatter(new SimpleFormatter());   
+        debugHandler.setLevel(Level.FINER);
+        logger.addHandler(debugHandler);
+        
+        FileHandler auditHandler = new FileHandler("log/audit.log", 1048576, 5, true);   
+        auditHandler.setFormatter(new SimpleFormatter());
+        auditHandler.setLevel(Level.INFO);
+        logger.addHandler(auditHandler);
+        
+        FileHandler errorHandler = new FileHandler("log/error.log", 1048576, 5, true);   
+        errorHandler.setFormatter(new SimpleFormatter());
+        errorHandler.setLevel(Level.SEVERE);
+        logger.addHandler(errorHandler);
+        
+        return logger;
     }
 }
