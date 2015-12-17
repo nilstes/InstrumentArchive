06ed0aeaa1fbc9893177b1b4fbaf36b5b8665407
stud-nilstes <nils.tesdal@hist.no>
2015-12-17 11:21:44 +0100

    Kan kjøre mysql og H2

--- src/main/java/db/Db.java
+++ src/main/java/db/Db.java
@@ -1,9 +1,7 @@
 package db;
 
-import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,53 +13,19 @@ import java.util.logging.Logger;
 public class Db {
 
     private static final Logger log = Logger.getLogger(Db.class.getName());
-    private static final String DB_NAME = "instruments";
     private static Db instance = new Db();
+    private static String dbUrl;
     
     private Db() {
         try {
-            Class.forName("org.h2.Driver");
-            Connection connection = getConnection();
-            try {
-                Statement statement = connection.createStatement();
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS user ( email VARCHAR(256) PRIMARY KEY, salt VARCHAR(256) NOT NULL, password VARBINARY(256) NOT NULL)");
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS instrument ( id VARCHAR(256) PRIMARY KEY, type VARCHAR(256), make VARCHAR(256), serial VARCHAR(256), product_no VARCHAR(256), description VARCHAR(256) )");
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS musician ( id VARCHAR(256) PRIMARY KEY, first_name VARCHAR(256), last_name VARCHAR(256) )");
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS instrument_type ( id VARCHAR(256) PRIMARY KEY, name VARCHAR(256) )");
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS instrument_state ( instrument_id VARCHAR(256), date DATETIME, state LONGVARCHAR, state_by_user VARCHAR(256))");
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS musician_instrument ( instrument_id VARCHAR(256), musician_id VARCHAR(256), out_at DATETIME, out_by_user VARCHAR(256), in_at DATETIME, in_by_user VARCHAR(256) )");
-                
-                statement.executeUpdate("MERGE INTO instrument_type values ('1', 'Kornett')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('2', 'Trompet')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('3', 'Klarinett')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('4', 'Klarinett (Bass)')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('5', 'Valthorn')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('6', 'Tuba')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('7', 'Baryton')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('8', 'Horn')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('9', 'Horn (Alt)')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('10', 'Horn (Tenor)')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('11', 'Horn (Flygel)')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('12', 'Obo')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('13', 'Fagott')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('14', 'Fløyte')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('15', 'Fløyte (Piccolo)')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('16', 'Saksofon')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('17', 'Saksofon (Alt)')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('18', 'Saksofon (Tenor)')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('19', 'Trombone')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('20', 'Trombone (Alt)')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('21', 'Trombone (Tenor)')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('22', 'Perkusjon (Rytmisk)')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('23', 'Perkusjon (Melodisk)')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('24', 'Saksofon (Baryton)')");
-                statement.executeUpdate("MERGE INTO instrument_type values ('25', 'Susafon')");
+            dbUrl = System.getProperty("db.url", "jdbc:h2:~/instruments?user=instruments");
+            String driver = "org.h2.Driver";
+            if(dbUrl.contains("mysql")) {
+                driver = "com.mysql.jdbc.Driver";
+            } 
+            Class.forName(driver);
 
-                statement.close();
-            } finally {
-                connection.close();
-            }
-            log.info("DB initialized!");
+            log.info("DB initialized using driver " + driver);
         } catch (Exception exception) {
             log.log(Level.SEVERE, "Failed to start DB", exception);
         }
@@ -74,34 +38,6 @@ public class Db {
     Connection getConnection() throws SQLException {
         // todo FIXME This is not a good way to get connections
         // Use a pool instead
-        return DriverManager.getConnection("jdbc:h2:~/" + DB_NAME, DB_NAME, "");
-    }
-    
-    public void dump(String filename) throws SQLException {
-        Connection connection = getConnection();
-        try {
-            PreparedStatement statement = connection.prepareStatement("SCRIPT TO ?");
-            statement.setString(1, filename);
-            statement.execute();
-        } catch (Exception exception) {
-            log.log(Level.SEVERE, "Failed to dump DB", exception);
-            throw exception;
-        } finally {
-            connection.close();
-        }
-    }
-    
-    public static void restore(String filename) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:h2:~/" + DB_NAME, DB_NAME, "");
-        try {
-            PreparedStatement statement = connection.prepareStatement("RUNSCRIPT FROM ?");
-            statement.setString(1, filename);
-            statement.execute();
-        } catch (Exception exception) {
-            log.log(Level.SEVERE, "Failed to restore DB", exception);
-            throw exception;
-        } finally {
-            connection.close();
-        }
+        return DriverManager.getConnection("db.url");
     }
 }
